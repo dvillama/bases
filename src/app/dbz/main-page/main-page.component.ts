299a97537352baaddab4b9279b7899fac66a6895
David Villanueva <davidvillanuevamarzo@gmail.com>
2022-08-26 12:45:47 +0200

    64. Depuración

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -25,19 +25,8 @@ export class MainPageComponent {
     poder: 0
   }
 
-  agregar( ) {
-
-    if( this.nuevo.nombre.trim().length === 0) {
-      return;
-    }
-
-    console.log(this.nuevo);
-    this.personajes.push( this.nuevo );
-    this.nuevo = {
-      nombre : '',
-      poder : 0
-    }
-    return;
+  agregarNuevoPersonaje( argumento: Personaje) {
+    this.personajes.push(argumento);
   }
 
 }
